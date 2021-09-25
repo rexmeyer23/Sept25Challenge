ae3c86ab9858cf1ebb60e89dc6870c34f797af80
Rex Meyer <armeyer1998@gmail.com>
2021-09-25 17:24:49 -0400

    Code Challenge committed

---
+++ CodeChallenge925/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge925
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cali = "supercalifragilisticexpialidocious";
+            foreach (char letter in cali)
+            {
+                Console.WriteLine(letter);
+            }
+            foreach (char letter in cali)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+
+                }
+                else
+                {
+                    Console.WriteLine("Not an i");
+                }
+            }
+            int num = 0;
+            foreach (char letter in cali)
+            {
+                num++;
+            }
+            int length = cali.Length;
+            Console.WriteLine(cali.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
